32c875a05005e8c475e1fdde9ab5018b9517938e
UtkarshSiddhpura <90092555+UtkarshSiddhpura@users.noreply.github.com>
2023-03-21 23:17:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var items = new Array();
 
 var WordList = [
+   {
+    name: "word1",
+    value: "Graduate",
+    type: "media",
+    list: { index: 1, name: "words" },
+    media: "text",
+    hint: "",
+  },
   {
     name: "word2",
     value: "Uniforms",
